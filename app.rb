63fc7c70758a1d71424127b466ba1fa32210da4e
Bryce Frazier and Emily Watkins <emily@>
2018-02-19 16:31:16 -0800

    issues with integration specs and routing

--- app.rb
+++ app.rb
@@ -1,7 +1,34 @@
-require("sinatra")
-require("sinatra/reloader")
-also_reload("lib/**/*.rb")
-require("./lib/task")
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/task')
+require('./lib/list')
+also_reload('lib/**/*.rb')
 require("pg")
 
-DB = PG.connect({:dbname => "to_do"})
+DB = PG.connect({:dbname => "to_do_test"})
+
+
+get("/") do
+  erb(:index)
+end
+
+get("/lists/new") do
+  erb(:list_form)
+end
+
+post("/lists") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:list_success)
+end
+
+get('/lists') do
+  @list = List.all()
+  erb(:lists)
+end
+
+get("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:lists)
+end
